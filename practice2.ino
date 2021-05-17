bd765992c3b44994291e563dcf678f6ca4cdfc45
bananawooyu <57030333+bananawooyu@users.noreply.github.com>
2021-05-17 10:05:16 +0900

    Add files via upload

--- practice2.ino
+++ practice2.ino
@@ -3,7 +3,7 @@
 #define DIO 9
 TM1637Display display(CLK, DIO);
 
-uint8_t data[] = { 0x0, 0x0, 0x0, 0x0 };                               
+uint8_t data[] = { 0x0, 0x0, 0x0, 0x0 };                                // 초기값으로 00:00 셋팅
 uint8_t segto;
 
 int Reset = 4;
@@ -18,18 +18,29 @@ void setup() {
   pinMode(Reset, INPUT_PULLUP);
   pinMode(Stop, INPUT_PULLUP);
   pinMode(Start, INPUT_PULLUP);
-  display.setBrightness(0xF);                              
+  display.setBrightness(0xFF);                               //최대 밝기 (1~7 밝기 조절)
 }
 
 void loop() {
   show();
-  // start 누르면 스탑워치 시작
 
-  // stop 누르면 스탑워치 정지
+  if (!digitalRead(Start) && state == 0) {
+    state = 1;
+    sound();
+  }
+  if (!digitalRead(Stop) && state == 1) {
+    state = 0;
+    sound();
+  }
+  if (!digitalRead(Reset)) {
+    state = 0;
+    readTime = 0;
+    sound();
+    delay(100);
+  }
+  if (state)
+    readTime++;
 
-  // reset 누르면 스탑워치 초기화
-
-  // 버튼 누를때 마다 소리 울림
 }
 
 void sound() {
@@ -39,18 +50,20 @@ void sound() {
 }
 
 void times() {
-  // 기존의 분,초를 초와 밀리초로 변환
-  // (1초 = 100밀리초)
+  Sec1 = (readTime / 100) / 10;
+  Sec2 = (readTime / 100) % 10;
+  Mil1 = (readTime % 100) / 10;
+  Mil2 = (readTime % 100) % 10;
 }
 
 void show() {
   times();
-  data[0] = display.encodeDigit(Sec1);                                   
+  data[0] = display.encodeDigit(Sec1);                                    // 초시계
   data[1] = display.encodeDigit(Sec2);
   data[2] = display.encodeDigit(Mil1);
   data[3] = display.encodeDigit(Mil2);
 
-  segto = 0x80 | display.encodeDigit(Sec2);                                
+  segto = 0x80 | display.encodeDigit(Sec2);                                 // 초시계용 ':'도트
   display.setSegments(data);
-  display.setSegments(&segto, 1, 1);                                    
+  display.setSegments(&segto, 1, 1);                                      // uint8_t segments[], uint8_length, uint8_pos length = 길이 / pos = 시작 위치
 }
